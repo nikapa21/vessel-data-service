010c880673dd5122c69ae9901c45b5c12969650d
nkaragounis <nkaragounis@lgcns.com>
2024-09-07 18:22:32 +0300

    Initial commit

---
+++ src/main/java/com/deepsea/vesseldataservice/VesselDataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.deepsea.vesseldataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VesselDataServiceApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(VesselDataServiceApplication.class, args);
+    }
+}
---
+++ src/test/java/com/deepsea/vesseldataservice/VesselDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepsea.vesseldataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VesselDataServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
